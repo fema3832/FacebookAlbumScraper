f2fb0f8891b43af6331e9fbbece85d94279d8558
Máté Fejes <88377040+fema3832@users.noreply.github.com>
2022-07-31 17:03:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 import urllib.request
+import ctypes
 import time
 import os
 
@@ -11,20 +15,21 @@ def dl_jpg(url, file_path, file_name):
     urllib.request.urlretrieve(url, fullpath)
     print(f'{file_name} downloaded to {file_path}')
 
-
-
 # * DOWNLOAD IMAGE =============================
 def dl_img(foldername):
-    path = r'C:\Users\user\Documents\chromedriver.exe'
+    firstname = ''
+    gotname = False
     oldimgname = ''
 
-    driver = webdriver.Chrome(path)
-    driver.get('album name')
+    ctypes.windll.kernel32.SetConsoleTitleW(f"Facebook album scraper | by fema3832")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    albumlink = input("album link: ")
+    driver.get(albumlink)
     driver.maximize_window()
     time.sleep(2)
 
     #* COOKIES =============================
-    okay = driver.find_element_by_xpath(r'//*[@id="facebook"]/body/div[2]/div[1]/div/div[2]/div/div/div/div[2]/div/div[2]/div')
+    okay = driver.find_element(By.XPATH, r'//*[@id="facebook"]/body/div[2]/div[1]/div/div[2]/div/div/div/div[2]/div/div[2]/div')
     okay.click()
 
     #* DOWNLOADING =============================
@@ -36,15 +41,19 @@ def dl_img(foldername):
 
         #* IMG SOURCE =============================
         time.sleep(2)
-        image = driver.find_element_by_xpath(r'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/div/div[1]/div/div[2]/div/div/div/img')
+        image = driver.find_element(By.XPATH, r'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/div/div[1]/div/div[2]/div/div/div/img')
         imgsrc = image.get_attribute('src')
         currentimgname = imgsrc[53:60]
 
+        if gotname == False:
+            firstname = currentimgname
+            gotname = True
+
         #* NEXT PAGE =============================
         time.sleep(2)
-        if currentimgname != oldimgname:
+        if currentimgname != oldimgname or currentimgname != firstname:
             dl_jpg(imgsrc, foldername, currentimgname)
-            nextpage = driver.find_element_by_xpath(r'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/div/div[1]/div/div[1]/div[3]/div')
+            nextpage = driver.find_element(By.XPATH, r'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/div/div[1]/div/div[1]/div[3]/div')
             driver.execute_script("arguments[0].click();", nextpage)
             oldimgname = currentimgname
         else:
